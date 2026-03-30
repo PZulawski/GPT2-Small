c25755ca9b553c24cc7673b9996e4d01cb071a48
Piotr Zulawski <piotrz@speechmatics.com>
2026-03-30 12:11:10 +0100

    add prefetched dataloader to read all training data onto chip

--- dataloader.py
+++ dataloader.py
@@ -1,7 +1,24 @@
-import tiktoken
 import torch
 from torch.utils.data import DataLoader
 
-class DataLoader(DataLoader):
-    def __init__(self):
-        
+class PrefetchDataLoader():
+    def __init__(self, dataloader: DataLoader, device):
+        batch_shape = next(iter(dataloader))[0].shape
+        data = list(dataloader)
+        self.data = torch.stack([batch[0] for batch in data if batch[0].shape == batch_shape]).to(device)
+        self.target = torch.stack([batch[1] for batch in data if batch[1].shape == batch_shape]).to(device)
+        self.ordering = torch.randperm(len(self.data))
+        self.counter = 0
+
+    def __getitem__(self, idx):
+        return self.data[idx], self.target[idx]
+    
+    def __next__(self):
+        if self.counter == len(self.data):
+            self.ordering = torch.randperm(len(self.data))
+            self.counter = 0
+        self.counter += 1
+        return self.__getitem__(self.ordering[self.counter - 1])
+    
+    def __len__(self):
+        return len(self.data)
